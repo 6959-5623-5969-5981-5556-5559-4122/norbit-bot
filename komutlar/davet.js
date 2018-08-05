a11368c1efe33b63f175c9adf592d3326e895cb4
TurkOglu1655 <42115393+TurkOglu1655@users.noreply.github.com>
2018-08-05 16:53:33 +0300

    Add files via upload

---
+++ komutlar/davet.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Link: https://discordapp.com/api/oauth2/authorize?client_id=459335363048112128&permissions=8&redirect_uri=https%3A%2F%2Fdiscordapi.com%2Fpermissions.html%23805314622&scope=bothttps://discordapp.com/api/oauth2/authorize?client_id=459335363048112128&permissions=8&redirect_uri=https%3A%2F%2Fdiscordapi.com%2Fpermissions.html%23805314622&scope=bot');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['botu ekle', 'botu davet et', 'botuekle', 'invite'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'davet',
+  description: 'Botun davet linkini gönderir.',
+  usage: 'davet'
+};
